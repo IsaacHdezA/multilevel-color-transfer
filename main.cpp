31f3ddaac6aa9aa3a8241894d97b5ba7e34f7020
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-04-16 00:34:31 -0600

    Implemented RGB_2_CIEXYZ function

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 
 #include "utils.hpp"
+
+Mat RGB_2_CIEXYZ(const Mat &);
 
 int main(void) {
     const string IMG_PATH = "./res/",
@@ -18,13 +21,44 @@ int main(void) {
                  IMG_TRG_FILENAME = IMG_PATH + IMG_TRG_NAME + IMG_EXT;
 
     Mat src = imread(IMG_SRC_FILENAME),
-        trg = imread(IMG_TRG_FILENAME);
+        trg = imread(IMG_TRG_FILENAME),
+        output = colorTransfer(src, trg);
 
-    imshow("Source image (" + IMG_SRC_NAME + ")", src);
-    imshow("Target image (" + IMG_TRG_NAME + ")", trg);
-
-    Mat output = colorTransfer(src, trg);
-    imshow("Resulting image", output);
+    Mat rgb_ciexyz = RGB_2_CIEXYZ(src);
+    cout << "RGB_2_CIEXYZ:" << '\n'
+         << "\t-> Original RGB pixel: " << src.at<Vec3b>(0, 0) << '\n'
+         << "\t-> Pixel in XYZ space: " << rgb_ciexyz.at<Vec3f>(0, 0) << endl;
 
     waitKey();
+}
+
+Mat RGB_2_CIEXYZ(const Mat &src) {
+    Mat output = Mat::zeros(1, 1, CV_32FC3);
+
+    if(!src.data || src.channels() == 1) {
+        cout << "\n\t! RGB_2_CIEXYZ: Image is empty or monochromatic. Should be three channels (BGR)." << endl;
+        return output;
+    }
+
+    const Mat M_CONV = (Mat_<float>(3, 3) <<
+        0.4124f, 0.3576f, 0.1805f,
+        0.2126f, 0.7152f, 0.0722f,
+        0.0193f, 0.1192f, 0.9505f
+    );
+
+    Mat srcFloat;
+    src.convertTo(srcFloat, CV_32FC3, 1.0 / 255.0, 0);
+
+    cout << "Normalized RGB pixel: " << srcFloat.at<Vec3f>(0, 0) << endl;
+
+    output = Mat::zeros(src.rows, src.cols, CV_32FC3);
+
+    for(int i = 0; i < srcFloat.rows; i++) {
+        Vec3f *row = (Vec3f *) srcFloat.ptr<Vec3f>(i),
+              *out = (Vec3f *)  output.ptr<Vec3f>(i);
+        for(int j = 0; j < srcFloat.cols; j++)
+            gemm(M_CONV, row[j], 1.0, Mat(), 0.0, out[j]);
+    }
+
+    return output;
 }
